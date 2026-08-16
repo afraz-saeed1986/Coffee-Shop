64a273e996e4f9bfc062aaf49728d06d57c9989a
Saeed Afraz <s.afraz@rayanbourse.ir>
2026-08-16 21:59:19 +0330

    Create Navbar Section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,5 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -10,8 +8,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="fa" dir='rtl'>
-      <body className={inter.className}>{children}</body>
+    <html lang="fa">
+      <body>{children}</body>
     </html>
   )
 }
