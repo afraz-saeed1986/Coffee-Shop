fe9ca0f68a7a5cc1ba0e97e3d0780771b533fb78
s.afraz <s.afraz@rayanbourse.ir>
2026-08-23 13:31:58 +0330

    Implemented User SignUp

--- src/components/templates/login-register/Register.js
+++ src/components/templates/login-register/Register.js
@@ -1,54 +1,110 @@
 import { useState } from "react";
 import styles from "./Register.module.css";
 import Sms from "./Sms";
+import swal from "sweetalert";
 
-const Register = ({showloginForm}) => {
-
+const Register = ({ showloginForm }) => {
   const [isRegisterWithPass, setIsRegisterWithPass] = useState(false);
   const [isRegisterWithOtp, setIsRegisterWithOtp] = useState(false);
 
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const hideOtpForm = () => setIsRegisterWithOtp(false);
+
+  const signUp = async () => {
+    // Validation
+    
+    const user = {name, phone, email, password};
+
+    const res = await fetch("/api/auth/signup", {
+      method: "POST",
+      headers: {
+        'Content-Type': "application/json"
+      }, 
+      body: JSON.stringify(user)
+    });
+
+    console.log(res);
+    if(res.status === 201){
+      swal({
+        title: "ثبت نام با موفقیت انجام شد",
+        icon: "success",
+        buttons:"ورود به پنل کاربری"
+      })
+    }
+    
+  };
 
   return (
     <>
-    {
-      !isRegisterWithOtp ? (
-          <>
-                          <div className={styles.form}>
-        <input className={styles.input} type="text" placeholder="نام" />
-        <input
-          className={styles.input}
-          type="text"
-          placeholder="شماره موبایل  "
-        />
-        <input
-          className={styles.input}
-          type="email"
-          placeholder="ایمیل (دلخواه)"
-        />
-        
-        {
-          isRegisterWithPass &&  
+      {!isRegisterWithOtp ? (
+        <>
+          <div className={styles.form}>
+            <input
+              value={name}
+              onChange={event => setName(event.target.value)}
+              className={styles.input}
+              type="text"
+              placeholder="نام"
+            />
+            <input
+              value={phone}
+              onChange={event => setPhone(event.target.value)}
+              className={styles.input}
+              type="text"
+              placeholder="شماره موبایل  "
+              F
+            />
+            <input
+              value={email}
+              onChange={event => setEmail(event.target.value)}
+              className={styles.input}
+              type="email"
+              placeholder="ایمیل (دلخواه)"
+            />
+
+            {isRegisterWithPass && (
               <input
-            className={styles.input}
-            type="password"
-            placeholder="رمز عبور"
-          />
-        }
-      
-        
-        <p onClick={() => setIsRegisterWithOtp(true)} style={{ marginTop: "1rem" }} className={styles.btn}>
-          ثبت نام با کد تایید
-        </p>
-        <button onClick={() => setIsRegisterWithPass(true)} style={{ marginTop: ".7rem" }} className={styles.btn}>
-          ثبت نام با رمزعبور
-        </button>
-        <p onClick={showloginForm} className={styles.back_to_login}>برگشت به ورود</p>
-      </div>
-      <p className={styles.redirect_to_home}>لغو</p>         
-          </>
-      ) : (<Sms hideOtpForm={hideOtpForm} />)
-    }
+                value={password}
+                onChange={event => setPassword(event.target.value)}
+                className={styles.input}
+                type="password"
+                placeholder="رمز عبور"
+              />
+            )}
+
+            <p
+              onClick={() => setIsRegisterWithOtp(true)}
+              style={{ marginTop: "1rem" }}
+              className={styles.btn}
+            >
+              ثبت نام با کد تایید
+            </p>
+            <button
+              onClick={() => {
+                if(isRegisterWithPass){
+                  signUp()
+                } else {
+                  setIsRegisterWithPass(true)
+                }
+                }}
+              style={{ marginTop: ".7rem" }}
+              className={styles.btn}
+            >
+              ثبت نام با رمزعبور
+            </button>
+            <p onClick={showloginForm} className={styles.back_to_login}>
+              برگشت به ورود
+            </p>
+          </div>
+          <p className={styles.redirect_to_home}>لغو</p>
+        </>
+      ) : (
+        <Sms hideOtpForm={hideOtpForm} />
+      )}
     </>
   );
 };
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,41 +1,63 @@
-import {hash, compare} from "bcryptjs";
+import { hash, compare } from "bcryptjs";
 import { sign, verify } from "jsonwebtoken";
 
 const hashPassword = async (password) => {
-    const hashedPassword = await hash(password, 12);
+  const hashedPassword = await hash(password, 12);
 
-    return hashedPassword;
-}
-
+  return hashedPassword;
+};
 
 const verifyPassword = async (password, hashedPassword) => {
-    const isValid = await compare(password, hashedPassword);
-
-    return isValid;
-}
+  const isValid = await compare(password, hashedPassword);
+  return isValid;
+};
 
 const generateAccessToken = (data) => {
-    const token = sign({...data}, process.env.AccessTokenSecretKey, {
-        expiresIn: "60s"
-    });
-    return token;
-}
+  const token = sign({ ...data }, process.env.AccessTokenSecretKey, {
+    expiresIn: "60s",
+  });
+  return token;
+};
 
 const verifyAccessToken = (token) => {
-    try {
-        const tokenPayload = verify(token, process.env.AccessTokenSecretKey);
-        return tokenPayload;
-    } catch (err) {
-        console.log("Verify Access Token Error ->", err);
-        return false;
-    }
-}
+  try {
+    const tokenPayload = verify(token, process.env.AccessTokenSecretKey);
+    return tokenPayload;
+  } catch (err) {
+    console.log("Verify Access Token Error ->", err);
+    return false;
+  }
+};
 
-const generateRefreshToken  = (data) => {
-    const token = sign({...data}, process.env.RefreshTokenSecretKey, {
-        expiresIn: "15d"
-    });
-    return token;
-}
+const generateRefreshToken = (data) => {
+  const token = sign({ ...data }, process.env.RefreshTokenSecretKey, {
+    expiresIn: "15d",
+  });
+  return token;
+};
 
-export {hashPassword, verifyPassword, generateAccessToken, verifyAccessToken, generateRefreshToken}
+const validateEmail = (email) => {
+    const pattern = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/g;
+    return pattern.test(email);
+};
+
+const validatePhone = (phone) => {
+    const pattern = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/g;
+    return pattern.test(phone);
+};
+
+const validatePassword = (password) => {
+    const pattern = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/g;
+    return pattern.test(password);
+};
+
+export {
+  hashPassword,
+  verifyPassword,
+  generateAccessToken,
+  verifyAccessToken,
+  generateRefreshToken,
+  validateEmail,
+  validatePhone,
+  validatePassword,
+};
